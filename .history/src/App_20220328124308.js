cc22e792d0e54ff797d7a2227a1b20163fa5afbb
Carolina Reedy <creedy@nerdsunite.me>
2022-03-28 13:21:34 -0600

    adding files

---
+++ .history/src/App_20220328124308.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+import { FaBasket } from "react-icons/fa";
+
+function App() {
+  return (
+    <div className="App">
+<div className="header-logo">
+  </div>
+  <div className="header-search"></div>
+  <div className="header-nav">
+    <div className="nav-item">
+    <div className="nav-item-one">Hello Guest</div>
+    <div className="nav-item-two">Sing In</div>
+    </div>
+    
+    <div className="nav-item">
+    <div className="nav-item-one">Your</div>
+    <div className="nav-item-two">Shop</div>
+    </div>
+    
+    <div className="nav-item">
+    <FaBasket fontSize="large"/>
+    <div className="nav-item-two">0</div>
+    </div>
+    </div>      
+
+      </div>)
+}
+
+export default App;
---
+++ .history/src/App_20220328130830.js
@@ -0,0 +1,34 @@
+
+import './App.css';
+
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header-logo">
+          <StorefrontIcon className="header-log-image" fontSize="large"/>
+      </div>
+    <div className="header-search"></div>
+    <div className="header-nav">
+      <div className="nav-item">
+      <div className="nav-item-one">Hello Guest</div>
+      <div className="nav-item-two">Sing In</div>
+    </div>
+    
+    <div className="nav-item">
+      <div className="nav-item-one">Your</div>
+      <div className="nav-item-two">Shop</div>
+    </div>
+    
+    <div className="nav-item">
+      <ShoppingBasketIcon fontSize="large"/>
+      <div className="nav-item-two">0</div>
+    </div>
+    </div>      
+
+      </div>)
+}
+
+export default App;
