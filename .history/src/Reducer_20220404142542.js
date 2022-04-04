7f9848fb692d557b1961499a0a112fbc098104cd
Carolina Reedy <creedy@nerdsunite.me>
2022-04-04 14:29:16 -0600

    added remove function

--- .history/src/Reducer_20220404142542.js
+++ .history/src/Reducer_20220404142542.js
@@ -15,7 +15,7 @@ const reducer = (state, action) => {
             }
             case "REMOVE_FROM_BASKET" :
             const index = state.basket.findIndex(
-                (basketItem) => basketItem.id === action.id
+                (basketItem) => basket.id === action.id
             );
             let newBasket = [...state.basket];
 
